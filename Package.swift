b93079a23d87db602ebf729a0eff212c61e868c4
Daniel Vershinin <d@sgml.me>
2021-09-10 06:50:21 +0300

    Initial commit.

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BaiduMTJ",
+    products: [
+        .library(
+            name: "BaiduMTJ",
+            targets: ["BaiduMTJ"])
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "BaiduMTJ",
+            dependencies: ["BaiduMTJCore", "BaiduMTJUserFeedback"]),
+        .binaryTarget(name: "BaiduMTJCore",
+                      path: "Resources/BaiduMTJCore.xcframework"),
+        .binaryTarget(name: "BaiduMTJUserFeedback",
+                      path: "Resources/BaiduMTJUserFeedback.xcframework")
+    ]
+)
